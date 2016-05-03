f595433e0f18a6ac7ec607104df5834c060fce60
Ramin Keene <raminkeene@mac.com>
2016-05-03 00:06:08 -0400

    move to a new machine

--- install/brew.sh
+++ install/brew.sh
@@ -1,13 +1,14 @@
 # Install Homebrew
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
 brew upgrade
 
 # Install packages
 
 apps=(
-  bash-completion2
+  awscli
+  bash-completion
   emacs
   git
   wget
